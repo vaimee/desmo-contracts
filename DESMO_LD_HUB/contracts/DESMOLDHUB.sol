55dd0bc55aafe002d48ca1314d1471b47444d347
Brenno Tondato <brenno.tondato@gmail.com>
2022-04-27 14:09:20 -0300

    Implementation and test or the ramdom TDD selection

--- DESMO_LD_HUB/contracts/DESMOLDHUB.sol
+++ DESMO_LD_HUB/contracts/DESMOLDHUB.sol
@@ -3,19 +3,43 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract desmo_ld_hub {
-    // What is the parameters required by the hub?
-    string[] private tddStorage;
-    mapping (string => string[]) private selectedTDDs;
-
+    
+    // TDDs storager
+    string[] internal tddStorage;
+    
+    // Ammount of TDDs to be selected
+    uint internal quant = 3;
+    
+    //Maaping to return the selected TDDs
+    mapping (uint256 => string[]) private selectedTDDs;
+    
     constructor() {
         console.log("Deploying the desmo HUB");
     }
 
+    function viewStorage()
+    public
+    returns (string[] memory) {
+        for (uint256 i = 0; i <= tddStorage.length - 1; i++) {
+            string memory s = tddStorage[i];
+            console.log("TDD at position '%d' is '%s' ", i, s);
+        }
+    }
+
+    // function viewSelected()
+    // public
+    // view {
+
+    // }
+
     // Register the TDD on the data struct
     function registerTDD(string memory tddID) 
-    external {
+    external 
+    returns (bool flag){
+        //tddStorage[msg.sender] = tddID;
         tddStorage.push(tddID);
-        console.log("TDD: '%s' stored", tddID);
+        console.log("Storing TDD '%s' at position '%d'", tddID, tddStorage.length-1);
+        return true;
     }
     
     function remove(uint256 index)
@@ -24,33 +48,46 @@ contract desmo_ld_hub {
         tddStorage.pop();
     }
 
-    // Remove the TDD of the data struct 
+    //Remove the TDD of the data struct 
     function unregisterTDD(string memory tddID) 
     external {    
         // We must verify the ownership of the TDD
         // find the index of the TDDid and then remove
-        for (uint256 i = 0; i < tddStorage.length - 1; i++) {
-            if(keccak256(bytes(tddStorage[i])) == keccak256(bytes(tddID))){
+        for (uint256 i = 0; i <= tddStorage.length - 1; i++) {
+            string memory s = tddStorage[i];
+            
+            if(keccak256(bytes(s)) == keccak256(bytes(tddID))){
                 remove(i);
-                console.log("TDD: '%s' removed", tddID);
-            }else{
-                console.log("TDD: '%s' not found", tddID);
+                console.log("TDD: '%s' removed and is '%s'", tddID, s);
             }
         }
     }
     
-    // TODO Return the ID of the ramdoly selected TDDs subset
-    // should be "payable" in the future
-    // should have an adaptative subset size accornding tho the ammont payed
-    function getNewRequestID() 
-    external 
-    returns (uint) {    
-        // create algorithm to scam the array of tdds randomly
-        // get the  key (how?)
+    function randomNumber(uint256 n) 
+    internal 
+    returns (uint) {
+        uint randomnumber = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, n))) % tddStorage.length;
+        return randomnumber;
     }
 
-    //  Returns the list of the TDDs subset
-    function getTDDByRequestID(string memory key) 
+    // Return the ID of the ramdoly selected TDDs subset
+    // should be "payable" in the future
+    function getNewRequestID() 
+    external 
+    returns (uint256) {    
+        uint256 key = uint256(uint160(address(msg.sender)));
+
+        for (uint256 i = 0; i <= quant; i++){
+            uint256 index = randomNumber(i);
+            selectedTDDs[key].push(tddStorage[index]);
+        }
+
+        console.log("This is the key '%s'", key);
+        return key;
+    }
+
+    // Returns the list of the TDDs subset
+    function getTDDByRequestID(uint256 key) 
     external
     view
     returns (string[] memory) {
--- DESMO_LD_HUB/test/index.ts
+++ DESMO_LD_HUB/test/index.ts
@@ -1,31 +1,96 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
+// describe("Greeter", function () {
+//   it("Should return the new greeting once it's changed", async function () {
     
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+//     const Greeter = await ethers.getContractFactory("Greeter");
+//     const greeter = await Greeter.deploy("Hello, world!");
+//     await greeter.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+//     expect(await greeter.greet()).to.equal("Hello, world!");
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+//     const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+//     // wait until the transaction is mined
+//     await setGreetingTx.wait();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+//     expect(await greeter.greet()).to.equal("Hola, mundo!");
+//   });
+// });
 
 describe("DESMO-LD HUB", function () {
-  it("Should store new DiD", async function () {
-    const DESMOHUB = await ethers.getContractFactory("DESMOLDHUB.sol");
+  it("Should register new DiD", async function () {
+    const DESMOHUB = await ethers.getContractFactory("desmo_ld_hub");
     const hub = await DESMOHUB.deploy();
     await hub.deployed();
 
+    const hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd1");
+    await hubRegisterTDD.wait();
+  });
+
+  it("Should show the storager", async function () {
+    const DESMOHUB = await ethers.getContractFactory("desmo_ld_hub");
+    const hub = await DESMOHUB.deploy();
+    await hub.deployed();
     
+    let hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd1");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd2");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd3");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd4");
+    await hubRegisterTDD.wait();
+    
+    let hubStorage = await hub.viewStorage();
+    await hubStorage.wait();
+  });
+
+  it("Should unregister TDD", async function () {
+    const DESMOHUB = await ethers.getContractFactory("desmo_ld_hub");
+    const hub = await DESMOHUB.deploy();
+    await hub.deployed();
+
+    let hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd1");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd2");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd3");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd4");
+    await hubRegisterTDD.wait();
+    
+    const hubUnregisterTDD = await hub.unregisterTDD("urn:thing:description:directory:tdd3");
+    await hubUnregisterTDD.wait();
+
+    let hubStorage = await hub.viewStorage();
+    await hubStorage.wait();
+  });
+
+  it("Should return a key for the selected TDDs", async function () {
+    const DESMOHUB = await ethers.getContractFactory("desmo_ld_hub");
+    const hub = await DESMOHUB.deploy();
+    await hub.deployed();
+
+    let hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd1");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd2");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd3");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd4");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd5");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd6");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd7");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd8");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd9");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd10");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd11");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd12");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd13");
+    hubRegisterTDD = await hub.registerTDD("urn:thing:description:directory:tdd14");
+    await hubRegisterTDD.wait();
+
+    let hubStorage = await hub.viewStorage();
+    await hubStorage.wait();
+
+    let idRequester = await hub.getNewRequestID();
+    await idRequester.wait(); 
 
   });
+
+  
+
 }); 
