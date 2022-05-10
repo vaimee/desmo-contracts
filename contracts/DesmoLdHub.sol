d530e61907f4beee42d46980a6f190a2984f0982
Brenno Tondato <brenno.tondato@gmail.com>
2022-05-10 17:54:45 -0300

    check TDD owner and existence, minor fixes, all tests

--- contracts/DesmoLdHub.sol
+++ contracts/DesmoLdHub.sol
@@ -7,45 +7,59 @@ contract desmo_ld_hub {
     // TDD struct
     struct TDD {
         string url;
+        address owner;
     }
     
     // TDDs storager
     //string[] internal tddStorage;
-    mapping(address =>TDD) private tddStorager;
+    mapping(address => TDD) private tddStorager;
 
+    //Maping to return the selected TDDs
+    mapping (uint256 => string[]) private selectedTDDs;
+    
     //register of all addresses registered; 
     address[] private addressRegisters;
 
     // TDD index counter
     uint256 private tddStoragerCounter = 0;
 
-    // 
-    mapping(address => uint256) private addressRef;
-    
     // Ammount of TDDs to be selected
     uint internal quant = 3;
     
-    //Maping to return the selected TDDs
-    mapping (uint256 => string[]) private selectedTDDs;
-    
-    
-    constructor() {
-        console.log("Deploying the desmo HUB");
+    constructor(){ 
     }
 
-    //TODO create modifiers to check the address
+    
+    function verify ()
+    internal
+    view 
+    returns (bool) {
+        if (bytes(tddStorager[msg.sender].url).length > 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    // Modifier to check if address is already on the tddStorager
+    modifier addressAlreadyInPlace() {
+        require( !verify(), "Sender already stored a value.");
+        _;
+    }
+    
+    // Modifier to check the msg.address == TDD owner
     modifier onlyOwner() {
-        require(addressRegisters[addressRef[msg.sender]-1] == msg.sender);
+        require(msg.sender == tddStorager[msg.sender].owner, "Not the TDD owner");
         _;
     } 
+    
+    // Modifier to ensure the retrival of a subset of TDDs > 0
+    modifier notEmptyTDDStorager () {
+        require(tddStoragerCounter > 0, "No TDD available. ");
+        _;
+    }
 
-    // //TODO create a modifier to check if address is already on the tddStorager
-    // modifier addressAlreadyInPlace() {
-    //     require(addressRef[msg.sender], "Sender already stored a value.");
-    //     _;
-    // }
-
-
+    // Function to view the selected subset of TDDs
     function viewSelected(uint256 id)
     public 
     view {
@@ -58,21 +72,22 @@ contract desmo_ld_hub {
     }
 
     //Register the TDD on the data struct
-    function registerTDD(TDD memory tdd) 
-    //addressAlreadyInPlace
+    function registerTDD(TDD memory tdd)
     external
-    returns (bool flag){
+    addressAlreadyInPlace
+    returns (uint256 index) {
         //tddStorage.push(tddID);
         tddStorager[msg.sender] = tdd;
         addressRegisters.push(msg.sender);
         tddStoragerCounter+=1;
-        addressRef[msg.sender] = tddStoragerCounter;
 
-        console.log(tddStoragerCounter);
+        //console.log("TDD Index: %d", tddStoragerCounter-1);
+        //console.log("TDD Storager counter: %d", tddStoragerCounter);
         
-        return true;
+        return tddStoragerCounter-1;
     }
     
+    // Function to remove the address from the addressRegisters
     function remove(uint256 index)
     internal {
         addressRegisters[index] = addressRegisters[tddStoragerCounter - 1];
@@ -81,49 +96,38 @@ contract desmo_ld_hub {
     }
 
     //Remove the TDD of the data struct 
-    function unregisterTDD()
-    // onlyOwner
-    external {    
-        //TODO We must verify the ownership of the TDD
+    function unregisterTDD(uint256 index)
+    external
+    onlyOwner {    
         delete tddStorager[msg.sender];
-        delete addressRef[msg.sender];
-        remove(addressRef[msg.sender]);
-        console.log(tddStoragerCounter);
-        
-
-        // find the index of the TDDid and then remove
-        // for (uint256 i = 0; i <= tddStorage.length - 1; i++) {
-        //     string memory s = tddStorage[i];
-            
-        //     if(keccak256(bytes(s)) == keccak256(bytes(tddID))){
-        //         remove(i);
-        //         console.log("TDD: '%s' removed and is '%s'", tddID, s);
-        //     }
-        // }
-        
+        remove(index);
+        //console.log("TDDs counter: '%d' ", tddStoragerCounter);   
     }
     
+    // Funtion to generate ramdon number inside the interval 0 - tddStoragerCounter
     function randomNumber(uint256 n) 
     internal
     view
     returns (uint) {
-        uint randomNumber = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, n))) %tddStoragerCounter;
-        return randomNumber;
+        return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, n))) % tddStoragerCounter;
     }
 
     // Return the ID of the ramdoly selected TDDs subset
     // should be "payable" in the future
+    // clould charge for more TDDs
+    // could charge for unique TDDs
     function getNewRequestID() 
-    external 
+    external
+    notEmptyTDDStorager
     returns (uint256) {    
         uint256 key = uint256(uint160(address(msg.sender)));
 
-        for (uint256 i = 0; i <= quant; i++){
+        for (uint256 i = 0; i <= quant - 1; i++){
             uint256 index = randomNumber(i);
             selectedTDDs[key].push(tddStorager[addressRegisters[index]].url);
         }
 
-        console.log("This is the key '%s'", key);
+        //console.log("This is the key '%s'", key);
         return key;
     }
 
--- test/index.ts
+++ test/index.ts
@@ -1,18 +1,49 @@
 import { expect } from "chai";
+import { Console } from "console";
 import { ethers } from "hardhat";
 
+
 describe("DESMO-LD HUB", function () {
+
   it("Should register new TDD", async function () {
     const DESMOHUB = await ethers.getContractFactory("desmo_ld_hub");
     const hub = await DESMOHUB.deploy();
     await hub.deployed();
+    
+    const [owner, addr1, addr2, addr3, addr4, addr5] = await ethers.getSigners();
 
     const TDD = {
-      url:"urn:thing:description:directory:tdd1"
+      url:"https://www.desmo.vaimee.it/2019/wot/tdd/v1/TDD:001",
+      owner: addr1.address
     }
 
-    const hubRegisterTDD = await hub.registerTDD(TDD);
+    const hubRegisterTDD = await hub.connect(addr1).registerTDD(TDD);
     await hubRegisterTDD.wait();
+
+    hubRegisterTDD
+
+  });
+
+  it("Should not register new TDD", async function () {
+    const DESMOHUB = await ethers.getContractFactory("desmo_ld_hub");
+    const hub = await DESMOHUB.deploy();
+    await hub.deployed();
+    
+    const [addr1] = await ethers.getSigners();
+
+    const TDD = {
+      url:"https://www.desmo.vaimee.it/2019/wot/tdd/v1/TDD:001",
+      owner: addr1.address
+    }
+    const TDD2 = {
+      url:"https://www.desmo.vaimee.it/2019/wot/tdd/v1/TDD:001",
+      owner: addr1.address
+    }
+
+    let hubRegisterTDD = await hub.connect(addr1).registerTDD(TDD);
+    await hubRegisterTDD.wait();
+
+    await expect(hub.connect(addr1).registerTDD(TDD2)).to.be.revertedWith('Sender already stored a value.');
   });
 
   it("Should unregister TDD", async function () {
@@ -20,48 +51,134 @@ describe("DESMO-LD HUB", function () {
     const hub = await DESMOHUB.deploy();
     await hub.deployed();
 
+    const [addr1, addr2] = await ethers.getSigners();
+
     const TDD = {
-      url:"urn:thing:description:directory:tdd1"
+      url:"https://www.desmo.vaimee.it/2019/wot/tdd/v1/TDD:001",
+      owner: addr1.address
     }
-    const hubRegisterTDD = await hub.registerTDD(TDD);
+
+    const hubRegisterTDD = await hub.connect(addr1).registerTDD(TDD);
     await hubRegisterTDD.wait();
 
-    const hubUnregisterTDD = await hub.unregisterTDD();
+    const TDD2 = {
+      url:"https://www.brenno.com.br/2019/wot/tdd/v1/TDD:002",
+      owner: addr2.address
+    }
+ 
+    const hubRegisterTDD2 = await hub.connect(addr2).registerTDD(TDD2);
+    await hubRegisterTDD2.wait();
+
+    const hubUnregisterTDD = await hub.connect(addr1).unregisterTDD(0);
     await hubUnregisterTDD.wait();
   });
 
-  it("Should return a key to retriece the selected TDDs", async function () {
+  it("Should fail to unregister TDD", async function () {
     const DESMOHUB = await ethers.getContractFactory("desmo_ld_hub");
     const hub = await DESMOHUB.deploy();
     await hub.deployed();
 
+    const [addr1, addr2] = await ethers.getSigners();
 
     const TDD = {
-      url:"urn:thing:description:directory:tdd1"
+      url:"https://www.desmo.vaimee.it/2019/wot/tdd/v1/TDD:001",
+      owner: addr2.address
     }
-    const hubRegisterTDD = await hub.registerTDD(TDD);
+
+    const hubRegisterTDD = await hub.connect(addr1).registerTDD(TDD);
     await hubRegisterTDD.wait();
 
-    let idRequester = await hub.getNewRequestID();
-    await idRequester.wait(); 
+    const TDD2 = {
+      url:"https://www.brenno.com.br/2019/wot/tdd/v1/TDD:002",
+      owner: addr2.address
+    }
+ 
+    const hubRegisterTDD2 = await hub.connect(addr2).registerTDD(TDD2);
+    await hubRegisterTDD2.wait();
 
+    await expect(hub.connect(addr1).unregisterTDD(0)).to.be.revertedWith('Not the TDD owner');
   });
 
+  it("Should return a key to retrieve the selected TDDs", async function () {
+    const DESMOHUB = await ethers.getContractFactory("desmo_ld_hub");
+    const hub = await DESMOHUB.deploy();
+    await hub.deployed();
+
+    const [addr1, addr2, addr3] = await ethers.getSigners();
+
+    const TDD = {
+      url:"https://www.desmo.vaimee.it/2019/wot/tdd/v1/TDD:001",
+      owner: addr1.address
+    }
+
+    const hubRegisterTDD = await hub.connect(addr1).registerTDD(TDD);
+    await hubRegisterTDD.wait();
+
+    const TDD2 = {
+      url:"https://www.brenno.com.br/2019/wot/tdd/v1/TDD:002",
+      owner: addr2.address
+    }
+
+    const hubRegisterTDD2 = await hub.connect(addr2).registerTDD(TDD2);
+    await hubRegisterTDD2.wait();
+
+    const TDD3 = {
+      url:"https://www.brenno.com.br/2019/wot/tdd/v1/TDD:003",
+      owner: addr3.address
+    }
+ 
+    const hubRegisterTDD3 = await hub.connect(addr3).registerTDD(TDD3);
+    await hubRegisterTDD3.wait();
+
+    let idRequester = await hub.connect(addr1).getNewRequestID();
+    await idRequester.wait(); 
+  });
+
+  it("Should fail to retrieve TDD from an empty TDD Storager", async function () {
+    const DESMOHUB = await ethers.getContractFactory("desmo_ld_hub");
+    const hub = await DESMOHUB.deploy();
+    await hub.deployed();
+
+    await expect(hub.getNewRequestID()).to.be.revertedWith('No TDD available. '); 
+  });
+  
   it("Should retrieve the selected TDDs by key", async function () {
     const DESMOHUB = await ethers.getContractFactory("desmo_ld_hub");
     const hub = await DESMOHUB.deploy();
     await hub.deployed();
     
+
+    const [addr1, addr2, addr3] = await ethers.getSigners();
+
     const TDD = {
-      url:"urn:thing:description:directory:tdd1"
+      url:"https://www.desmo.vaimee.it/2019/wot/tdd/v1/TDD:001",
+      owner: addr1.address
     }
-    const hubRegisterTDD = await hub.registerTDD(TDD);
+
+    const hubRegisterTDD = await hub.connect(addr1).registerTDD(TDD);
     await hubRegisterTDD.wait();
 
-    let idRequester = await hub.getNewRequestID();
+    const TDD2 = {
+      url:"https://www.brenno.com.br/2019/wot/tdd/v1/TDD:002",
+      owner: addr2.address
+    }
+
+    const hubRegisterTDD2 = await hub.connect(addr2).registerTDD(TDD2);
+    await hubRegisterTDD2.wait();
+
+    const TDD3 = {
+      url:"https://www.brenno.com.br/2019/wot/tdd/v1/TDD:003",
+      owner: addr3.address
+    }
+ 
+    const hubRegisterTDD3 = await hub.connect(addr3).registerTDD(TDD3);
+    await hubRegisterTDD3.wait();
+
+    let idRequester = await hub.connect(addr1).getNewRequestID();
     await idRequester.wait(); 
 
-    let subsetTDDs = await hub.viewSelected("1390849295786071768276380950238675083608645509734");
+    //let subsetTDDs = await hub.connect(addr1).viewSelected("1390849295786071768276380950238675083608645509734");
     // call funtion to return the tdd subset list 
   });
+
 }); 
